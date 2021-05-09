da68ca824f8eac4d0a69881c11525f0d47c3a98b
digthedill <popssly@gmail.com>
2021-05-09 16:30:47 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
+  const [data, setData] = useState(null)
+
+  // app that shows the closest setlist to the current date
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(data)
+
+  const fetchData = () => {
+    return axios({
+      method: "GET",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "x-api-key": "SPZ6CgBYu2cfEv1myvPklRkKqGJPU2UiX2-v",
+      },
+
+      url:
+        "https://cors-anywhere.herokuapp.com/https://api.setlist.fm/rest/1.0/artist/6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6/setlists",
+    })
+      .then((res) => setData(res.data))
+      .catch((e) => console.error(e))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{data && JSON.stringify(data.setlist[0], null, 4)}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
